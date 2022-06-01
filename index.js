2969f77573a10b7925db6d291ee7cfc7f0b71979
Vedkumar Gupta <93338056+aesstechindia@users.noreply.github.com>
2022-06-01 19:47:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const port = process.env.PORT || 8080;
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World! $hostname'))
+app.get('/', (req, res) => res.send('Hello Openshift World! $hostname'))
 
 app.get('/readiness', (req, res) => res.send('Ready !!'))
 
